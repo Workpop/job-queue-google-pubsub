2de8619200599724bc55706828c63d74a67995bf
Pavel Pavlov <pavel@workpop.com>
2019-07-10 14:51:52 +1200

    fix(publisher): Add support for plain values

--- src/publisher.ts
+++ src/publisher.ts
@@ -1,4 +1,5 @@
 import { PubSub } from '@google-cloud/pubsub';
+import { isBuffer, isObject, toString } from 'lodash';
 import { IPublisher, IQueueConfig } from './types';
 
 export class JobQueuePublisher implements IPublisher {
@@ -11,6 +12,12 @@ export class JobQueuePublisher implements IPublisher {
 
   public publish(topicId: string, message: any): Promise<string> {
     const topic = this.pubsubClient.topic(topicId);
+    if (isBuffer(message)) {
+      return topic.publish(message);
+    }
+    if (!isObject(message)) {
+      return topic.publish(Buffer.from(toString(message)));
+    }
     return topic.publishJSON(message, {
       'content-type': 'application/json',
     });
